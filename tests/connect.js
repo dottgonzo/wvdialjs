183d91487451ea55e75e74836a9089a619844cd7
Dario Caruso <darioyzf@gmail.com>
2015-10-14 01:49:26 +0200

    oki

--- tests/connect.js
+++ tests/connect.js
@@ -2,6 +2,7 @@ var verb=require('verbo'),
 Wv=require('../index.js'),
 wvdialjs=new Wv('/etc/wvdial.conf'),
 isOnline = require('is-online'),
+netw= require('netw'),
 timerdaemon=require('timerdaemon');
 
 
@@ -9,6 +10,9 @@ timerdaemon=require('timerdaemon');
 wvdialjs.connect()
 
 timerdaemon.post(5000,function(){
+  netw.data().then(function(doc){
+    console.log(doc.network)
+  })
   isOnline(function(err, online) {
       verb(online);
       //=> true
