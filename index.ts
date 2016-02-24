950585c71657e80c7e7135cac1b208c577184d9c
Dario Caruso <darioyzf@gmail.com>
2016-02-24 15:45:06 +0100

    up

--- index.ts
+++ index.ts
@@ -8,10 +8,17 @@ let exec = require('promised-exec');
 let Tail = require('always-tail');
 
 let verb = require('verbo');
+
+let mobilestatus = false;
+
 //spawn = require('child_process').spawn,
 
 
-
+interface IConfOpt {
+    verbose?: boolean;
+    dev?: any;
+    provider: IProviderCF;
+};
 
 
 interface IProviderCF {
@@ -21,6 +28,15 @@ interface IProviderCF {
     phone?: string;
     username?: string;
     password?: string;
+
+}
+interface IProvider {
+
+    label?: string;
+    apn: string;
+    phone: string;
+    username: string;
+    password: string;
 
 }
 // modprobe usbserial
@@ -100,7 +116,7 @@ function connect(configFilePath: string, watch?: boolean, device?: string) {
         let lncount = 0;
 
         function wvconnect() {
-
+            mobilestatus = false;
             if (lncount > 200) {
 
 
@@ -223,7 +239,7 @@ function connect(configFilePath: string, watch?: boolean, device?: string) {
                 // setTimeout(function () {
                 //   exec('ip route add default dev ppp0')
                 // }, 30000);
-
+                mobilestatus = true;
 
                 fs.writeFileSync(wvdialerr, "");
 
@@ -233,18 +249,26 @@ function connect(configFilePath: string, watch?: boolean, device?: string) {
 
                 console.log('ppp connected')
 
-                if (!watch) {
-                    tail.unwatch();
-                    resolve(true);
+            //    if (!watch) {
+            //        tail.unwatch();
+             //       resolve(true);
 
-                }
+            //    }
 
 
 
             } else if (lncount > 200) {
+                mobilestatus = false;
+
+                if (!watch) {
+                    tail.unwatch();
+                    reject(true);
+
+                } else {
+                    hwrestart("reboot");
+                }
 
 
-                hwrestart("reboot");
 
 
 
@@ -288,20 +312,61 @@ function connect(configFilePath: string, watch?: boolean, device?: string) {
 }
 
 
+function setprov(configFilePath, provider: IProviderCF) {
+
+
+
+
+    if (!provider.phone) provider.phone = '*99#';
+    if (!provider.username) provider.username = '';
+    if (!provider.password) provider.password = '';
+
+
+    this.provider = provider;
+
+    return new Promise<{ success?: boolean }>(function(resolve, reject) {
+        if (provider.apn) {
+            setstring(configFilePath, 'Init3', 'AT+CGDCONT=1,"ip","' + provider.apn + '",,0,0').then(function() {
+                console.log('ok apn');
+                if (provider.phone) {
+                    setstring(configFilePath, 'Phone', provider.phone);
+                }
+                if (provider.username) {
+                    setstring(configFilePath, 'Username', provider.username);
+                }
+                if (provider.password) {
+                    setstring(configFilePath, 'Password', provider.password);
+                }
+                resolve({ success: true });
+            })
+        } else {
+            reject("no apn");
+        }
+    })
+
+};
+
+
 export =class WvDial {
     configFilePath: string;
-    provider: string;
-    constructor(public path: string) {
+    provider: IProviderCF;
+    device: any;
+    constructor(path: string, device?: string) {
         if (path) {
             this.configFilePath = path; // /etc/wvdial.conf
         } else {
             this.configFilePath = '/etc/wvdial.conf';
         }
+        if (device) {
+            this.device = device; // /etc/wvdial.conf
+        } else {
+            this.device = false; // /etc/wvdial.conf
+        }
     };
 
-    connect(watch?: boolean, dev?: string) {
+    connect(watch?: boolean) {
         let configFilePath = this.configFilePath;
-
+        let dev = this.device;
         return new Promise<boolean>(function(resolve, reject) {
             console.log('connection');
 
@@ -338,32 +403,7 @@ export =class WvDial {
     };
 
     setProvider(provider: IProviderCF) {
-        let configFilePath = this.configFilePath;
-
-
-        if (!provider.phone) provider.phone = '*99#';
-        if (!provider.username) provider.username = '';
-        if (!provider.password) provider.password = '';
-
-        return new Promise<{ success?: boolean }>(function(resolve, reject) {
-            if (provider.apn) {
-                setstring(configFilePath, 'Init3', 'AT+CGDCONT=1,"ip","' + provider.apn + '",,0,0').then(function() {
-                    console.log('ok apn');
-                    if (provider.phone) {
-                        setstring(configFilePath, 'Phone', provider.phone);
-                    }
-                    if (provider.username) {
-                        setstring(configFilePath, 'Username', provider.username);
-                    }
-                    if (provider.password) {
-                        setstring(configFilePath, 'Password', provider.password);
-                    }
-                    resolve({ success: true });
-                })
-            } else {
-                reject("no apn");
-            }
-        })
+        return setprov(this.configFilePath, provider)
 
     };
 
@@ -371,17 +411,61 @@ export =class WvDial {
         return allstrings(this.configFilePath);
     };
 
-    setParam(key:string, val:string) {
+    setParam(key: string, val: string) {
         return setstring(this.configFilePath, key, val);
     };
 
-    getParam(param:string) {
+    getParam(param: string) {
         return getstring(this.configFilePath, param);
     };
 
+    status() {
+
+        return mobilestatus
+
+    }
+    setdev(device: string) {
+        let setdev = this.device;
+        let configFilePath = this.configFilePath;
+        return new Promise<{ success?: boolean }>(function(resolve, reject) {
+            lsusbdev().then(function(data) {
+                let devto: any = false;
+                for (var i = 0; i < data.length; i++) {
+                    var usb = data[i];
 
 
+                    if (usb.type == 'serial' && usb.hub == device && !devto) {
+                        console.log('set ' + usb.dev)
+
+
+                        devto = usb.dev;
+
+
+                    }
+                }
+
+
+                if (devto) {
+                    setstring(configFilePath, 'Modem', devto).then(function() {
+                        setdev = device;
+                        resolve({ success: true });
+                    }).catch(function(err) {
+                        reject({ error: 'error on setstring ' });
+                    })
+
+                } else {
+
+                    reject({ error: 'error on modem ' });
+
+                }
+
+            })
+
+        })
+
+    }
     configure(provider: IProviderCF) {
+        let device = this.device;
         let configFilePath = this.configFilePath;
         return new Promise<{ success?: boolean }>(function(resolve, reject) {
             if (provider) {
@@ -389,25 +473,76 @@ export =class WvDial {
                 if (!provider.username) provider.username = '';
                 if (!provider.password) provider.password = '';
 
-                exec('echo "[Dialer Defaults]" > ' + configFilePath).then(function() {
-                    exec('echo \'Init3 = AT+CGDCONT=1,"ip","' + provider.apn + '",,0,0\' >> ' + configFilePath).then(function() {
-                        exec('echo "Phone = ' + provider.phone + '" >> ' + configFilePath).then(function() {
-                            exec('echo "Username = ' + provider.username + '" >> ' + configFilePath).then(function() {
-                                exec('echo "Password = ' + provider.password + '" >> ' + configFilePath).then(function() {
-                                    exec('wvdialconf ' + configFilePath).then(function() {
-                                        resolve({ success: true });
-                                    }).catch(function(err) {
-                                        reject({ error: 'error on modem ' });
+                if (device) {
+
+                    setprov(this.configFilePath, provider).then(function() {
+                        lsusbdev().then(function(data) {
+                            let devto: any = false;
+                            for (var i = 0; i < data.length; i++) {
+                                var usb = data[i];
+
+
+                                if (usb.type == 'serial' && usb.hub == device && !devto) {
+                                    console.log('set ' + usb.dev)
+
+
+                                    devto = usb.dev;
+
+
+                                }
+                            }
+
+
+                            if (devto) {
+                                setstring(configFilePath, 'Modem', devto).then(function() {
+
+                                    resolve({ success: true });
+                                }).catch(function(err) {
+                                    reject({ error: 'error on setstring ' });
+                                })
+
+                            } else {
+
+                                reject({ error: 'error on modem ' });
+
+                            }
+
+                        })
+
+                    }).catch(function(err) {
+                        reject({ error: 'error on setprov ' });
+                    })
+
+                } else {
+
+                    exec('echo "[Dialer Defaults]" > ' + configFilePath).then(function() {
+                        exec('echo \'Init3 = AT+CGDCONT=1,"ip","' + provider.apn + '",,0,0\' >> ' + configFilePath).then(function() {
+                            exec('echo "Phone = ' + provider.phone + '" >> ' + configFilePath).then(function() {
+                                exec('echo "Username = ' + provider.username + '" >> ' + configFilePath).then(function() {
+                                    exec('echo "Password = ' + provider.password + '" >> ' + configFilePath).then(function() {
+
+
+                                        exec('wvdialconf ' + configFilePath).then(function() {
+                                            resolve({ success: true });
+                                        }).catch(function(err) {
+                                            reject({ error: 'error on modem ' });
+                                        })
+
+
+
+
                                     })
                                 })
                             })
                         })
-                    })
 
-                }).catch(function(err) {
-                    reject({ error: 'error on open ' + configFilePath });
+                    }).catch(function(err) {
+                        reject({ error: 'error on open ' + configFilePath });
 
-                });
+                    });
+
+
+                }
 
 
 
--- run/connect-tre.js
+++ run/connect-tre.js
@@ -3,10 +3,22 @@ var Wv=require('../index.js');
 var wvdialjs=new Wv('/etc/wvdial.conf');
 
 
-
-
+wvdialjs.configure({"label":"Tre Ricaricabile","apn":"tre.it","phone":"*99#","username":"tre","password":"tre"}).then(function(data){
 wvdialjs.connect(true).catch(function(err){
 
 
   verb(err,'error','connection')
 });
+setTimeout(function(){
+    
+   console.log(wvdialjs.status()) 
+    
+},50000)
+}).catch(function(err){
+  verb(err,"error","error")
+})
+
+
+
+
+
