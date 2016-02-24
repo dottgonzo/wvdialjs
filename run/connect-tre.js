4113895851bb5b00b497e63c988da353d6701299
Dario Caruso <darioyzf@gmail.com>
2016-02-24 10:22:24 +0100

    up

--- run/connect-tre.js
+++ run/connect-tre.js
@@ -5,10 +5,7 @@ var wvdialjs=new Wv('/etc/wvdial.conf');
 
 
 
-wvdialjs.connect(true).then(function(){
-  verb('ok','info','connection')
-
-}).catch(function(err){
+wvdialjs.connect(true).catch(function(err){
 
 
   verb(err,'error','connection')
