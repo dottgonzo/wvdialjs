8ed6525183922ea556c640dc84a4e6864216e8fe
Dario Caruso <darioyzf@gmail.com>
2016-02-24 16:00:22 +0100

    up

--- run/connectwithhub.ts
+++ run/connectwithhub.ts
@@ -3,8 +3,7 @@ var Wv=require('../index.js');
 var wvdialjs=new Wv('/etc/wvdial.conf');
 
 
-
-
+wvdialjs.configure({"label":"Tre Ricaricabile","apn":"tre.it","phone":"*99#","username":"tre","password":"tre"}).then(function(data){
 wvdialjs.connect(true,"1-1.4").then(function(){
   verb('ok','info','connection')
 
@@ -13,3 +12,14 @@ wvdialjs.connect(true,"1-1.4").then(function(){
 
   verb(err,'error','connection')
 });
+setTimeout(function(){
+    
+   console.log(wvdialjs.status()) 
+    
+},240000)
+}).catch(function(err){
+  verb(err,"error","error")
+})
+
+
+
